7773d6a2b06c0401acdd95820a2704357d27169c
Brandon Canfield <sneelhorses@gmail.com>
2019-05-28 21:25:01 -0400

    Init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+
+const PORT = process.env.PORT || 4000;
+
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+app.get('/:room', function(req, res) {
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+	});
+	socket.on('fragment', function(fragment) {
+		socket.broadcast.emit('fragment', fragment);
+	});
+	socket.on('buffer', function(buffer) {
+		socket.broadcast.emit('buffer', buffer);
+	});
+});
+
+http.listen(PORT, function(){
+	console.log('listening on *:' + PORT);
+});
